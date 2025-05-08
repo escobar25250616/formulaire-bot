ea730dd1c6f0152fed613d5e6a4e1d407e7ee9a9
Votre Nom <votre.email@example.com>
2025-05-08 19:42:34 +0200

    Premier commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+TELEGRAM_BOT_TOKEN = '8186336309:AAFMZ-_3LRR4He9CAg7oxxNmjKGKACsvS8A'
+TELEGRAM_CHAT_ID = '6297861735'
+
+@app.route('/')
+def home():
+    return open('index.html').read()
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = f"📥 Nouveau formulaire:\n👤 Nom: {name}\n📧 Email: {email}"
+
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    data = {'chat_id': TELEGRAM_CHAT_ID, 'text': message}
+    requests.post(url, data=data)
+
+    return "Merci ! Votre message a été envoyé."
+
+if __name__ == '__main__':
+    app.run(debug=True)
